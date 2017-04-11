81f40d26b5d5d8be24f15cf73707b40d982671af
Matt Le <ml9951@rit.edu>
2017-04-11 17:53:22 -0400

    Updating scraper to download using requests
    This allows us to download into any folder instead of the
    default directory that Chrome decides

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import pdb
 import pandas
 from time import sleep
+import requests
 
 driver = webdriver.Chrome()
 
@@ -28,6 +29,21 @@ rows = map(lambda x: list(x.findAll('td')), table.findAll('tr'))
 
 df = pandas.DataFrame(filter(lambda x: len(x) != 0, rows), columns = columns)
 
+def download(url, cookies):
+	formData = {}
+	inputs = driver.find_elements_by_xpath('//div[contains(@class, "region-content")]//form//input')
+	for inp in inputs:
+		formData[inp.get_attribute('name')] = inp.get_attribute('value')
+
+	local_filename = '../files/' + url.split('/')[-1]
+	print('Downloading %s' % local_filename)
+	r = requests.post(url, cookies=cookies, data=formData)
+	with open(local_filename, 'wb') as f:
+		for chunk in r.iter_content(chunk_size=1024): 
+			if chunk: # filter out keep-alive new chunks
+				f.write(chunk)
+	return 
+
 for link in df['Title'].apply(lambda x: x.find('a').attrs['href']):
 	driver.get(base + link + '/GRADING/ALL')
 
@@ -42,6 +58,10 @@ for link in df['Title'].apply(lambda x: x.find('a').attrs['href']):
 		confirmation = driver.find_element_by_name('confirmation')
 		confirmation.send_keys(True)
 		confirmation.click()
-		submit = driver.find_element_by_id('edit-submit')
-		submit.click()
-		sleep(1) #Sleep for a second so as to not overload their server
+
+		all_cookies = driver.get_cookies()
+		cookies = {}
+		for s_cookie in all_cookies:
+			cookies[s_cookie['name']] = s_cookie['value']
+
+		download(base+zl, cookies)
