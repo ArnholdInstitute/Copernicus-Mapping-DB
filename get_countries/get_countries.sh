9a04750e0c0351dcb8ef853fd03cbb74d17cd4f6
Matt Le <ml9951@rit.edu>
2017-04-12 10:35:27 -0400

    Add script to pull country admin boundaries

---
+++ get_countries/get_countries.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+URL=http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
+
+if [ ! -f countries.zip ]; then
+	wget $URL -O countries.zip
+fi
+
+if [ ! -f ne_50m_admin_0_countries.shp ]; then
+	unzip countries.zip
+fi
+
+shp2pgsql -s 4326 -I -D -d ne_50m_admin_0_countries.shp | psql copernicus
